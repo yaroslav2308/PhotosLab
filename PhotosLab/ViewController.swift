e4c1fdf2c841c53bb2f17175b4d5a8c2a4f05f0f
yaroslav2308 <yaroslav.monastyrev@gmail.com>
2021-11-02 22:44:27 +0300

    Initial Commit

---
+++ PhotosLab/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PhotosLab
+//
+//  Created by Yaroslav Monastyrev on 02.11.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
